1c6a1e63412a1085f0860b5d028f6d4f26137647
Remi Duval <contact@remiduval.com>
2020-07-17 11:40:51 +0200

    Tile

--- _includes/assets/js/map.js
+++ _includes/assets/js/map.js
@@ -10,10 +10,10 @@ var map = new L.map('map', mapOptions);
 // Creating a Layer object
 var layer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZHV2YWxyZW1pIiwiYSI6ImNrY3EwbjdpODEwbXYzM3BiNG1wamlnYjcifQ.f5jpxqWg8DfhZ2ZtyxWqSg', {
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'duvalremi/ckcq0t29c0zbn1ip6s4s58ltx',
+    id: 'duvalremi/ckcq128li0k5l1iuoyhoah895',
     tileSize: 512,
     zoomOffset: -1,
     detectRetina: true
