e9dcaa519ce33901de75bdb3b4bbbaf4a42c7528
Remi Duval <contact@remiduval.com>
2020-07-17 11:33:44 +0200

    Changing tile

--- _includes/assets/js/map.js
+++ _includes/assets/js/map.js
@@ -8,12 +8,19 @@ var mapOptions = {
 var map = new L.map('map', mapOptions);
 
 // Creating a Layer object
-var layer = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
-    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    minZoom: 1,
-    maxZoom: 19,
+var layer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZHV2YWxyZW1pIiwiYSI6ImNrY3EwbjdpODEwbXYzM3BiNG1wamlnYjcifQ.f5jpxqWg8DfhZ2ZtyxWqSg', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'duvalremi/ckcq0t29c0zbn1ip6s4s58ltx',
+    tileSize: 512,
+    zoomOffset: -1,
     detectRetina: true
 });
+
+
+
 
 // Adding layer to the map
 map.addLayer(layer);
